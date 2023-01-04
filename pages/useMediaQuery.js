febed109c19dcd539bbb24e35c308cac3e992691
Thanachot W <Thanachot.wongmetin@gmail.com>
2023-01-04 09:53:35 +0700

    update use media query

--- pages/useMediaQuery.js
+++ pages/useMediaQuery.js
@@ -1,27 +1,20 @@
-import React from 'react';
+export const useMediaQuery = (width) => {
+  const [targetReached, setTargetReached] = useState(false)
 
+  const updateTarget = useCallback((e) => {
+    if (e.matches) setTargetReached(true)
+    else setTargetReached(false)
+  }, [])
 
-export const useMediaQuery = (width)=> {
-  const [targetReached, setTargetReached] = React.useState(false);
-
-  const updateTarget = React.useCallback((e) => {
-    if (e.matches) {
-      setTargetReached(true);
-    } else {
-      setTargetReached(false);
-    }
-  }, []);
-  React.useEffect(() => {
-    const media = window.matchMedia(`(max-width: ${width}px)`);
-    media.addListener(updateTarget);
+  useEffect(() => {
+    const media = window.matchMedia(`(max-width: ${width}px)`)
+    media.addEventListener('change', updateTarget)
 
     // Check on mount (callback is not called until a change occurs)
-    if (media.matches) {
-      setTargetReached(true);
-    }
+    if (media.matches) setTargetReached(true)
 
-    return () => media.removeListener(updateTarget);
-  }, []);
+    return () => media.removeEventListener('change', updateTarget)
+  }, [])
 
-  return targetReached;
-};
+  return targetReached
+}
