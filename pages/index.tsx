f2dd10d018db7b5e76dc4ac3813389cec1c066e5
Thanachot W <Thanachot.wongmetin@gmail.com>
2023-01-08 20:08:23 +0700

    lastedupdate

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,11 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
 import { useRouter } from 'next/router'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image'
 import { Card, Grid, Text, Button, Container, Badge, Avatar, Row, Col, Spacer } from "@nextui-org/react";
+import { Navbar } from "@nextui-org/react";
+
 import {
   SiReact, SiNextdotjs,
   SiGoland,
@@ -22,20 +24,59 @@ import {
   SiPhp,
   SiHtml5,
   SiCss3,
-  SiJavascript,
-  SiJquery,
-  SiGit,
-  SiGithub,
-  SiVisualstudiocode,
   SiFigma,
-  SiAdobeillustrator,
-  SiAdobephotoshop,
   SiDocker,
 
 } from 'react-icons/si';
 import { FaNodeJs } from 'react-icons/fa';
-export default function Home() {
+
+
+export default function App() {
+  
+  const list = [
+    {
+      role: "UX/UI Design and IoT Developer",
+      title: "Meplug",
+      img: "/mepluglogo.png",
+      description : "Meplug is a smart plug that can be controlled by voice and mobile application.",
+
+    },
+    {
+      role: "Fullstack Developer",
+      title: "Mypark",
+      img: "/myparklogo.png",
+      description : "Mypark is a smart parking system that can be show parking lot's free and can used in mobile application.",
+    },
+    {
+      role: "Fronted Developer",
+      title: "Today",
+      img: "/todaylogo.png",
+      description : "Today App is a mobile application that can send message to your friend and can use in mobile application."
+    },
+    {
+      role: "Fronted Developer",
+      title: "Welendar",
+      img: "/welendarlogo.png",
+      description : "Welendar is a mobile application that can show your schedule and can use in mobile application."
+    },
+    {
+      role: "UX/UI Design",
+      title: "Gather",
+      img: "/gatherlogo.png",
+      description : " Gather is a mobile application that can booking a activities in university "
+    },
+    {
+      role: "UX/UI Design",
+      title: "Carish",
+      img: "/carishlogo.png",
+      description : " Carish is a mobile application that can show your health quality and can chat with doctor"
+    }
+  ];
   const router = useRouter()
+
+  function handleClick() {
+    router.push('/projects');
+  }
   return (
     <>
       <Head>
@@ -46,6 +87,7 @@ export default function Home() {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"></link>
       </Head>
       <main className={styles.main}>
+
         <Image
           className={styles.profile}
           src="/profile_Thanachot.jpg"
@@ -60,8 +102,7 @@ export default function Home() {
           <code className={styles.code}>Software Engineer · UX/UI Design · Photographer </code>
           <p className={styles.introduce}>
             I am passionate about designing user interfaces and user experiences,
-            and I a
-            <div className="card-panel"></div>lso have a strong background in software engineering.I am always looking for new challenges
+            and I also have a strong background in software engineering.I am always looking for new challenges
             and opportunities to make innovation. </p>
 
           <code className={styles.code}>
@@ -90,10 +131,7 @@ export default function Home() {
           <code className={styles.code} >
             Skills
           </code>
-          <Container gap={1}>
-            <Row gap={0} style={{ justifyContent: 'center' }} >
-
-            </Row>
+          <Container gap={1} wrap={'wrap'}>
             <Row gap={0} style={{ justifyContent: 'center' }} >
               <Grid.Container gap={2} css={{ color: "$text", marginTop: "$1" }} justify={'center'}>
                 <Grid> <Avatar squared icon={<SiReact />} /></Grid>
@@ -118,44 +156,87 @@ export default function Home() {
                 <Grid> <Avatar squared icon={<SiFigma />} /></Grid>
               </Grid.Container>
             </Row>
-            <code className={styles.code} >
-              Work Experiences
-            </code>
-            <Row gap={0} style={{ justifyContent: 'center' }}  >
-              <Card css={{ m: '$6', p: "$6", mw: "400px" }}>
-                <Card.Header>
-                  <img
-                    alt="nextui logo"
-                    src="https://media.licdn.com/dms/image/C560BAQHnOtmYTOvWPw/company-logo_200_200/0/1671785697940?e=1681344000&v=beta&t=RqenS7HhQ8gYG3oLAQjT2uXH_OBaqOzZoga2gI4bdT4"
-                    width="54px"
-                    height="54px"
-                  />
-                  <Grid.Container css={{ pl: "$6" }}>
-                    <Grid xs={12}>
-                      <Text h4 css={{ lineHeight: "$xs" }}>
-                        Digital Trainnee
-                      </Text>
-                    </Grid>
-                    <Grid xs={12}>
-                      <Text css={{ color: "$accents8" }}>WEDO SCG Sement</Text>
-                    </Grid>
-                  </Grid.Container>
-                </Card.Header>
-                <Card.Body css={{ py: "$2" }}>
-                  <Text>
-                    Research and Development IoT smart outlet and application services for ev user live in condominuim.
-                  </Text>
-                </Card.Body>
-              </Card>
-
+            <br />
+            <Row gap={0} style={{ justifyContent: 'center' }} >
+              <code className={styles.code} >
+                Work Experiences
+              </code>
             </Row>
+            <Row gap={0} style={{ justifyContent: 'center', }} fluid>
+            <Card css={{ m: '$6', p: "$6", mw: "400px" }}>
+                  <Card.Header>
+                    <img
+                      alt="nextui logo"
+                      src="https://media.licdn.com/dms/image/C560BAQHnOtmYTOvWPw/company-logo_200_200/0/1671785697940?e=1681344000&v=beta&t=RqenS7HhQ8gYG3oLAQjT2uXH_OBaqOzZoga2gI4bdT4"
+                      width="54px"
+                      height="54px"
+                    />
+                    <Grid.Container css={{ pl: "$6" }}>
+                      <Grid xs={12}>
+                        <Text h4 css={{ lineHeight: "$xs" }}>
+                          Digital Trainee
+                        </Text>
+                      </Grid>
+                      <Grid xs={12}>
+                        <Text css={{ color: "$accents8" }}> WEDO - SCG Cement-Building Materials</Text>
+                      </Grid>
+                    </Grid.Container>
+                  </Card.Header>
+                  <Card.Body css={{ py: "$2" }}>
+                    <Text>
+                      Research and Development IoT smart outlet and application services for ev user live in condominuim.
+                    </Text>
+                  </Card.Body>
+                </Card>
+            </Row>
+            <br />
 
+            <Row gap={0} style={{ justifyContent: 'center' }} >
+              <code className={styles.code} >
+                Experiences in Projects
+              </code>
+              
+            </Row>
+            <br />
+            <Row gap={0} style={{ justifyContent: 'center', }} fluid>
+              
+              <Grid.Container gap={2} justify="flex-start">
+                {list.map((item, index) => (
+                  <Grid xs={12} sm={2} key={index}>
+                  <Card css={{ m: '$6', p: "$6", mw: "400px" }} isPressable>
+                  <Card.Header>
+                    <img
+                      alt="nextui logo"
+                      src={item.img}
+                      width="64px"
+                      height="64px"
+                    />
+                    <Grid.Container css={{ pl: "$6" }}>
+                      <Grid xs={12} justify="space-between" >
+                        <Text h5 css={{ lineHeight: "$xs" }} >
+                          {item.title} 
+                        </Text>
+                        <Badge color="success" variant={"flat"}> complete </Badge> 
+                      </Grid>
+                      <Grid xs={12} css={{textAlign:"left"}}>
+                        <Text css={{ color: "$accents8" }} size={12}> {item.role} </Text>
+                      </Grid>
+                    </Grid.Container>
+                  </Card.Header>
+                  <Card.Body css={{ py: "$2" }}>
+                    <Text size={12}>
+                    {item.description}</Text>
+                  </Card.Body>
+                  <Card.Footer>
+                  <Button className={styles.btn2} onClick={handleClick}>View</Button>
+                  </Card.Footer>
+                </Card>
+                  </Grid>
+                ))}
+              </Grid.Container>
+            </Row>
           </Container>
-
         </div>
-
-
-
       </main>
     </>
   )
