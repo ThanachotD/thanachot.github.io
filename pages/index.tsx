519a225cc5a45380eed430bfb98d22e1213c93b1
Thanachot W <Thanachot.wongmetin@gmail.com>
2023-01-08 20:43:52 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -84,7 +84,7 @@ export default function App() {
         <meta name="firstpage" content="introduce myself by thanachot" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/thanachot.ico" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"></link>
+        
       </Head>
       <main className={styles.main}>
 
