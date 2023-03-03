e03d6a7546c2ceb0a9e2e99e450b283f4dfaf65b
Thanachot W <Thanachot.wongmetin@gmail.com>
2023-03-03 19:17:11 +0700

    Last Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,8 +43,8 @@ export default function Home() {
             
             <div className='text-[#CFD1D3] text-sm m-2 font-thin'> </div>
             <div className="flex items-center">
-           <Link className=" text-[#CFD1D3]  mx-3" href={"https://github.com/ThanachotD"}><SiGithub size={25} className='hover:text-[#000]'/></Link>
-           <Link className=" text-[#CFD1D3]  mx-3" href={'https://www.linkedin.com/in/thanachot-wongmetin-94bb49210/'}><SiLinkedin  size={25} className='hover:text-[#007AEA]'/>  </Link>
+           <Link aria-label="Read more about Github" className=" text-[#CFD1D3]  mx-3" href={"https://github.com/ThanachotD"}><SiGithub size={25} className='hover:text-[#000]'/></Link>
+           <Link aria-label="Read more about Linked" className=" text-[#CFD1D3]  mx-3" href={'https://www.linkedin.com/in/thanachot-wongmetin-94bb49210/'}><SiLinkedin  size={25} className='hover:text-[#007AEA]'/>  </Link>
             <button onClick={getResume} className="bg-[#08FDD8] mx-4 mr-2 hover:bg-[#FFF]  text-[#000000] font-bold text-sm py-2 px-6 rounded-full  flex flex-row animate-bounce">
             <FaFile size={18} className='mx-1 '/> Resume </button> 
             </div>
